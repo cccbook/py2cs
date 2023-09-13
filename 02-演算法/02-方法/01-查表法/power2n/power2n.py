efa2f7ec00ebaaf7c6574831556e486b106472a9
ccckmit <ccckmit@gmail.com>
2023-09-13 11:55:44 +0800

    add power2n

---
+++ 02-演算法/02-方法/01-查表法/power2n/power2n.py
@@ -0,0 +1,20 @@
+# 方法 1
+def power2n(n):
+    return 2**n
+
+# 方法 2a：用遞迴
+def power2n(n):
+    pass
+    # power2n(n-1)+power2n(n-1)
+
+# 方法2b：用遞迴
+def power2n(n):
+    pass
+    # 2*power2n(n-1)
+
+# 方法 3：用遞迴+查表
+def power2n(n):
+    pass
+    # if ....
+    # power2n(n-1)+power2n(n-1) 
+
