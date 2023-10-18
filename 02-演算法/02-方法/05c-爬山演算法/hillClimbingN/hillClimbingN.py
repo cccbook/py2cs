738c2bbda78dfad5fb0ba99e31d454ccbfb6837a
ccckmit <ccckmit@gmail.com>
2023-10-18 10:55:06 +0800

    add hillclimbingN

--- 02-演算法/02-方法/05c-爬山演算法/hillClimbingN/hillClimbingN.py
+++ 02-演算法/02-方法/05c-爬山演算法/hillClimbingN/hillClimbingN.py
@@ -4,7 +4,7 @@ def hillClimbing(f, p, h=0.01):
     failCount = 0                    # 失敗次數歸零
     while (failCount < 10000):       # 如果失敗次數小於一萬次就繼續執行
         fnow = f(p)                  # fxy 為目前高度
-        fneighbor = neighbor(f, p)
+        fneighbor = neighbor(f, p, h)
         if fneighbor >= fnow:        # 如果移動後高度比現在高
             fnow = fneighbor         #   就移過去
             print('p=', p, 'f(p)=', fnow)
@@ -17,4 +17,4 @@ def f(x, y, z):
     #return -1 * ( x*x -2*x + y*y +2*y - 8 )
     return -1*(x**2+y**2+z**2)
 
-hillClimbing(f, [0,0,0])
+hillClimbing(f, [2,1,3])
