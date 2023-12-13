8fd54e154c36670d66eb9a7b7025e718c52689ea
ccckmit <ccckmit@gmail.com>
2023-12-13 13:22:15 +0800

    add fequal for lambda

--- 06-計算理論/code/03-丘奇的函數/07-unsolvable/02-fequal/fequal.py
+++ 06-計算理論/code/03-丘奇的函數/07-unsolvable/02-fequal/fequal.py
@@ -1,12 +1,13 @@
-def fequal(f1, f2, n):
-    a = f1(n)
-    b = f2(n)
+def fequal(A, B):
+    a = A()
+    b = B()
     return a==b
 
-def f1(n):
-    return n * n
+def f1():
+    return 10 * 10
 
-def f2(n):
+def f2():
+    n = 10000
     s = 0
     for _ in range(n):
         for _ in range(n):
@@ -14,7 +15,6 @@ def f2(n):
                 for _ in range(n):
                     s = s+1
 
-print('fequal(f1,f2,3)=', fequal(f1,f2, 3))
-print('fequal(f1,f2,1000)=', fequal(f1, f2, 1000))
+print('fequal(f1,f2)=', fequal(f1,f2))
 
 
