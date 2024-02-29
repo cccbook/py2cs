91330d189875f11cc05d3a7b63fe31c76a2c723e
ccckmit <ccckmit@gmail.com>
2024-02-29 12:13:54 +0800

    add tsp.py

---
+++ 03-人工智慧/02-優化算法/01-傳統優化方法/01-優化/01-爬山演算法/03-通用的爬山框架/tsp.py
@@ -0,0 +1,25 @@
+citys = [
+    (0,0),(0,1),(0,2),(0,3),
+    (1,0),(1,3),
+    (2,0),(2,3),
+    (3,0),(3,3)
+]
+
+path = [i for i in range(len(citys))]
+print(path)
+
+def distance(p1, p2):
+    print('p1=', p1)
+    x1, y1 = p1
+    x2, y2 = p2
+    return ((x2-x1)**2+(y2-y1)**2)**0.5
+
+def pathLength(p):
+    dist = 0
+    plen = len(p)
+    for i in range(plen):
+        dist += distance(citys[p[i]], citys[p[(i+1)%plen]])
+    return dist
+
+print('pathLength=', pathLength(path))
+
