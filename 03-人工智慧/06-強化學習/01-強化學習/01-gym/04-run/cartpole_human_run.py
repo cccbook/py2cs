ae137c19443cdeca58208430250989c5dff7e819
ccckmit <ccckmit@gmail.com>
2024-05-02 12:01:31 +0800

    add cartpole_mypolicy_run.py

--- 03-人工智慧/06-強化學習/01-強化學習/01-gym/04-run/cartpole_human_run.py
+++ 03-人工智慧/06-強化學習/01-強化學習/01-gym/04-run/cartpole_human_run.py
@@ -2,12 +2,15 @@ import gymnasium as gym
 env = gym.make("CartPole-v1", render_mode="human") # 若改用這個，會畫圖
 # env = gym.make("CartPole-v1", render_mode="rgb_array")
 observation, info = env.reset(seed=42)
-for _ in range(100):
+score = 0
+for _ in range(1000):
    env.render()
    action = env.action_space.sample()  # this is where you would insert your policy
    observation, reward, terminated, truncated, info = env.step(action)
-   print('observation=', observation)
+   # print('observation=', observation)
+   score += reward
    if terminated or truncated:
       observation, info = env.reset()
-      print('done')
+      print('done, score=', score)
+      score = 0
 env.close()
