3137f418d466e475bddd933ada580c52f8b45431
ccckmit <ccckmit@gmail.com>
2024-10-15 11:01:00 +0800

    finish xyGradient.py

--- 02-演算法/02-方法/05e-梯度下降法/02-梯度/xyGradient.py
+++ 02-演算法/02-方法/05e-梯度下降法/02-梯度/xyGradient.py
@@ -4,3 +4,9 @@ def grad(f, p, h=0.001):
     dfx = f([x+h,y])-f([x,y])
     dfy = f([x,y+h])-f([x,y])
     return [dfx/h,dfy/h]
+
+def f(p):
+    [x,y] = p
+    return x * x + y * y
+
+print('grad(f, [1,1])=', grad(f, [1,2]))
